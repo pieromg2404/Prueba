53487af4b5a4ee8a70447bdddece94d94d003fa4
pieromg2404 <152648854+pieromg2404@users.noreply.github.com>
2024-03-13 17:27:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
